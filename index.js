2633840af4007d2b9ae7fb0153d725f1ec945e25
Alex Miller <fotoflo@gmail.com>
2022-05-09 23:21:32 +0800

    code cleanup

--- index.js
+++ index.js
@@ -11,17 +11,15 @@ const config = {
 
 const fs = require('fs-extra')
 const chokidar = require('chokidar')
+
 const cloudinary = require('cloudinary');
+cloudinary.config(config.cloudinary);
+
 const notifier = require('node-notifier');
 
-function pbcopy(data) {
-  var proc = require('child_process').spawn('pbcopy'); 
-  proc.stdin.write(data); proc.stdin.end();
-}
 
+// ensure the directory exists
 fs.ensureDirSync(config.homeDir);
-
-cloudinary.config(config.cloudinary);
 
 // Initialize watcher.
 const watcher = chokidar.watch(config.homeDir, {
@@ -34,23 +32,31 @@ watcher
   .on('add', path => newFileHandler(path))
 
 const newFileHandler = path => {
-  console.log( `uploading ${path}`)
-  
-  const uploadCompleteHandler = (err, result) => {
-    if(err){
-      console.log("error", err)
-      return
-    }
+  console.log( `Uploading ${path}`)
 
-    console.log(`uploaded to ${result.secure_url}`)
-    notifier.notify(`upload complete ${result.secure_url}`);
-    pbcopy(result.secure_url)
-  }
-
-  cloudinary.v2.uploader.upload(path, {
+  cloudinaryOptions = {
     public_id: path.split("/")
         .pop() // get the end of the path
         .replace(/ /g, "-") // replace spaces with -
         .slice(0, -4) // remove extra .png
-  }, uploadCompleteHandler);
+  }
+
+  cloudinary.v2.uploader.upload(path, cloudinaryOptions, uploadCompleteHandler);
+}
+
+const uploadCompleteHandler = (err, result) => {
+  if(err){
+    console.log("Error: ", err)
+    return
+  }
+
+  console.log(`Uploaded to ${result.secure_url}`)
+  notifier.notify(`Upload complete ${result.secure_url}`);
+  copyToClipboard(result.secure_url)
+}
+
+//copy to clipboard
+const copyToClipboard = (data) => {
+  var proc = require('child_process').spawn('pbcopy'); 
+  proc.stdin.write(data); proc.stdin.end();
 }
