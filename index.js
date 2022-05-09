1e6a73c6b590ffc58009e601d1656240ae2ba8b5
Alex Miller <fotoflo@gmail.com>
2022-05-09 22:38:39 +0800

    initial commit

---
+++ index.js
@@ -0,0 +1,56 @@
+require('dotenv').config()
+
+const config = {
+  homeDir: '/Users/fotoflo/screenshots',
+  cloudinary: { 
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
+    api_key: process.env.CLOUDINARY_API_KEY, 
+    api_secret: process.env.CLOUDINARY_API_SECRET 
+  }
+}
+
+const fs = require('fs-extra')
+const chokidar = require('chokidar')
+const cloudinary = require('cloudinary');
+const notifier = require('node-notifier');
+
+function pbcopy(data) {
+  var proc = require('child_process').spawn('pbcopy'); 
+  proc.stdin.write(data); proc.stdin.end();
+}
+
+fs.ensureDirSync(config.homeDir);
+
+cloudinary.config(config.cloudinary);
+
+// Initialize watcher.
+const watcher = chokidar.watch(config.homeDir, {
+  ignored: /.+(?<!\.png)$/gm, // ignore non png
+  persistent: true,
+  ignoreInitial: true
+});
+
+watcher
+  .on('add', path => newFileHandler(path))
+
+const newFileHandler = path => {
+  console.log( `uploading ${path}`)
+  
+  const uploadCompleteHandler = (err, result) => {
+    if(err){
+      console.log("error", err)
+      return
+    }
+
+    console.log(`uploaded to ${result.secure_url}`)
+    notifier.notify(`upload complete ${result.secure_url}`);
+    pbcopy(result.secure_url)
+  }
+
+  cloudinary.v2.uploader.upload(path, {
+    public_id: path.split("/")
+        .pop() // get the end of the path
+        .replace(/ /g, "-") // replace spaces with -
+        .slice(0, -4) // remove extra .png
+  }, uploadCompleteHandler);
+}
---
+++ readme.md
@@ -0,0 +1,20 @@
+# SCREENSHOT UPLOADER
+
+Simple tool for mac to 
+ - monitor a folder for screenshots,
+ - upload to cloudinary
+ - send the link to your clipboard
+
+how to use:
+1. create a [cloudinary account](https://cloudinary.com/invites/lpov9zyyucivvxsnalc5/yqdxmbz83uyohiz4ebig?t=default) (use my link and i'll get credits)
+2. create a screenshots folder somewhere
+4. set your default file location
+   1. Open the Keyboard control panel -> shortcuts -> screenshots
+   2. Set the Screenshot and Recording Options shortcut
+   3. Run it once and select the `save to` option
+5. Move the .env-example file to .env
+6. Fill in the variables from cloudinary and your recording options
+7. Run the node script
+
+# to do: 
+figure out how to get the node script to run at startup and take the envirenment variables by some UI
